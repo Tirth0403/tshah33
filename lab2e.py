d477bdb5c17ad2778e69544c1c03ba07ceec6388
tirth0403 <shahtirth4803@gmail.com>
2024-05-31 11:35:37 -0400

    commit eith lab2 files

---
+++ lab2e.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+# Author:Tirth Amitkumar Shah
+# Author ID: 175983212
+# Date Created: 2024/05/30
+
+# Initialize the timer object with a value of 10
+timer = 10
+
+# While loop that repeats until timer equals 0
+while timer > 0:
+    print(timer)
+    timer -= 1
+
+# Print the final message when the timer reaches 0
+print("blast off!")
+
